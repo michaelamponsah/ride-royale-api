1f821f879ded151d2c692fec0fe2e3bda5668534
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-05-12 10:01:06 +0000

    Resolve validation error in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  validates username:, presence: true, length: { minimum: 3, maximum: 20 }
   has_many :reservations, dependent: :destroy
   has_many :cars, through: :reservations, dependent: :destroy
+
+  validates :username, presence: true, length: { minimum: 3, maximum: 20 }
 end
