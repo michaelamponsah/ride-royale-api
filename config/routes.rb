311ec45d49ad3397d21be9489243036bc3b3a8b6
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-05-12 17:40:40 +0300

    Create routs cars

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  #cars end point
+  namespace :api do
+    namespace :v1 do
+      resources :cars do
+        get 'user_cars', on: :collection
+        delete 'delete', on: :member
+      end
+    end
+  end
 end
