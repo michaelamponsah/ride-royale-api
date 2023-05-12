90e54c180c12312e8dfb82a253476b5213c008b0
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-05-12 09:24:36 +0000

    Implement validations and associations on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates username:, presence: true, length: {minimum: 3, maximum: 20} 
+  has_many :reservations, dependent: :destroy
+  has_many :cars, through: :reservations, dependent: :destroy
 end
