9e85d127098cc331456fe685155add1ce9d5672d
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-05-12 13:27:21 +0300

    Fix linter

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,9 +1,9 @@
 class Car < ApplicationRecord
-    belongs_to :user
-    has_many :reservations
-  
-    validates :model, presence: true
-    validates :make, presence: true
-    validates :picture, presence: true
-    validates :price, presence: true, numericality: { greater_than: 0 }
+  belongs_to :user
+  has_many :reservations
+
+  validates :model, presence: true
+  validates :make, presence: true
+  validates :picture, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
 end
