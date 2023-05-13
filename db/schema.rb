9f543e3a0cb4d9f614ff3b9009335b8251a6e34a
Saeqa Sultani <saeqasultani@gmail.com>
2023-05-13 22:13:11 +0430

    Creat the model for reservation and added the relations and validations to it

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_12_102426) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_13_172337) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_12_102426) do
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
+  create_table "reservations", force: :cascade do |t|
+    t.date "date"
+    t.string "city"
+    t.bigint "user_id", null: false
+    t.bigint "car_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_reservations_on_car_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.datetime "created_at", null: false
@@ -32,4 +43,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_12_102426) do
   end
 
   add_foreign_key "cars", "users"
+  add_foreign_key "reservations", "cars"
+  add_foreign_key "reservations", "users"
 end
