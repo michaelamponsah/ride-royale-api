0e083f955b27a00eac2c55cf181b2c9fc0212047
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-05-15 19:06:26 +0300

    Update routs combination

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,19 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :cars, only: [:index, :create, :destroy, :show] do
+        member do
+          delete :delete
+        end
+
+        collection do
+          get :user_cars
+        end
+      end
       
       resources :user, only: [:index, :create] do
       resources :reservations, only: [:index, :create, :destroy, :show]
     end
   end
 end
- 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  #cars end point
-  namespace :api do
-    namespace :v1 do
-      resources :cars do
-        get 'user_cars', on: :collection
-        delete 'delete', on: :member
-      end
-    end
-  end
 end
