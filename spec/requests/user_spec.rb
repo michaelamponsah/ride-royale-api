c14f272bb81922d73fe7b48020d40af82505c0e7
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-05-24 21:16:25 +0300

    Update api docmentations

---
+++ spec/requests/user_spec.rb
@@ -0,0 +1,48 @@
+# spec/requests/sessions_spec.rb
+require 'swagger_helper'
+
+RSpec.describe 'users', type: :request do
+  path '/register/{username}/{email}' do
+    # You'll want to customize the parameter types...
+    parameter name: 'username', in: :path, type: :string, description: 'username'
+    parameter name: 'name', in: :path, type: :string, description: 'name'
+    parameter name: 'email', in: :path, type: :string, description: 'email'
+
+    post('register user') do
+      response(200, 'successful') do
+        let(:username) { '123' }
+        let(:name) { '123' }
+        let(:email) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+
+  path '/login/{username}' do
+    # You'll want to customize the parameter types...
+    parameter name: 'username', in: :path, type: :string, description: 'username'
+
+    get('login user') do
+      response(200, 'successful') do
+        let(:username) { '123' }
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
